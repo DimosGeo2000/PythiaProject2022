1dbf53b161d1ae9bab9e3360f92ef52c4ede734c
Thodoris-Diamantidis <49129562+Thodoris-Diamantidis@users.noreply.github.com>
2021-11-23 23:32:36 +0200

    Added new method sumOfTwo takes two integer and returns sum +tested new method on main with an example

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,13 +4,22 @@
  * @author Τεο
  */
 public class MainClass {
-
+    
     public static void main(String[] args) {    
+            int  first = 100;
+            int  second = 1540;
         showMyText();
+        System.out.println("SumOfTwo is : " + sumOfTwo(first,second));
     }
     
     public static void showMyText(){
         System.out.println("Hello World!");
+    }
+    
+    public static int sumOfTwo(int one , int two){
+        int sum = 0;
+        sum = one + two;
+        return sum;
     }
     
 }
